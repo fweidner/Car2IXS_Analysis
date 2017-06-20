1880460b7816825cb130f7be39cf0e60e40fb514
Florian Weidner <info@florianweidner.de>
2017-06-20 18:29:31 +0200

    Minor update which skips normality.

--- FinalStatistics.py
+++ FinalStatistics.py
@@ -14,9 +14,10 @@ import numpy as np
 
 import statistics
 
-def CalcNonParametric_MannWhitneyWithShapiro(list1, list2, my_alternative='two-sided', printHint=False):
-    print ('\t\tnormality list1 (Shapiro) = ' + str(scipy.stats.shapiro(list1)))
-    print ('\t\tnormality list2 (Shapiro) = ' + str(scipy.stats.shapiro(list2)))
+def CalcNonParametric_MannWhitneyWithShapiro(list1, list2, my_alternative='two-sided', printHint=False, printNorm=False):
+    if (printNorm):
+        print ('\t\tnormality list1 (Shapiro) = ' + str(scipy.stats.shapiro(list1)))
+        print ('\t\tnormality list2 (Shapiro) = ' + str(scipy.stats.shapiro(list2)))
     print('\t\t' + str(scipy.stats.mannwhitneyu(list1, list2, alternative=my_alternative)))
     print ('\t\tMean l1 :' + str(statistics.mean(list1)) + '\tStDev l1: ' + str(statistics.stdev(list1)))
     print ('\t\tMean l2 : ' + str(statistics.mean(list2))+ '\tStDev l2: ' + str(statistics.stdev(list2)))
@@ -28,9 +29,10 @@ def CalcNonParametric_MannWhitneyWithShapiro(list1, list2, my_alternative='two-s
         print('\t\tShapiro     :' + 'If P > 0.05, it may be assumed that the data have a normal distribution.')
     #http://www.randalolson.com/2012/08/06/statistical-analysis-made-easy-in-python/
 
-def CalcParametric_WelshWithShapiroAndLevene(list1, list2, printHint=False):
-    print ('\t\tnormality list1 (Shapiro) = ' + str(scipy.stats.shapiro(list1)))
-    print ('\t\tnormality list2 (Shapiro) = ' + str(scipy.stats.shapiro(list2)))
+def CalcParametric_WelshWithShapiroAndLevene(list1, list2, printHint=False, printNorm=False):
+    if (printNorm):
+        print ('\t\tnormality list1 (Shapiro) = ' + str(scipy.stats.shapiro(list1)))
+        print ('\t\tnormality list2 (Shapiro) = ' + str(scipy.stats.shapiro(list2)))
  
     equalvar = scipy.stats.levene(list1, list2, center='mean')
       
@@ -38,6 +40,7 @@ def CalcParametric_WelshWithShapiroAndLevene(list1, list2, printHint=False):
         print ('\t\t' + str(scipy.stats.ttest_ind(list1, list2, equal_var=False)) + '; Welch: '+ str(equalvar[1])) #Welch
     else: 
         print ('\t\t' + str(scipy.stats.ttest_ind(list1, list2, equal_var=True))+ '; t-test: '+str(equalvar[1]))
+        # a negative sign implies tahat the sample mean is less than the hypothesized mean
     
     print ('\t\tMean l1 :' + str(statistics.mean(list1)) + '\tStDev l1: ' + str(statistics.stdev(list1)))
     print ('\t\tMean l2 : ' + str(statistics.mean(list2))+ '\tStDev l2: ' + str(statistics.stdev(list2)))
