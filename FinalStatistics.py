61e80fc07e478e3877fc557c7d4a3deefec906cb
fweidner <info@florianweidner.de>
2017-06-24 22:23:10 +0200

    Update on log scale: uses symlog.

--- FinalStatistics.py
+++ FinalStatistics.py
@@ -27,26 +27,29 @@ rcParams['figure.figsize'] = 16/2,12/2
 import statistics
 
 def CalcNonParametric_MannWhitneyWithShapiro(list1, list2, my_alternative='two-sided', printHint=False, printNorm=False):
-    if (printNorm):
+    if (False):
         print ('\t\tnormality list1 (Shapiro) = ' + str(scipy.stats.shapiro(list1)))
         print ('\t\tnormality list2 (Shapiro) = ' + str(scipy.stats.shapiro(list2)))
     
     print('\t\t' + str(scipy.stats.mannwhitneyu(list1, list2, alternative=my_alternative)))
-    
-    if (False):
+    prefix = ''
+    if (True):
         val1= statistics.median(list1)
         val2 = statistics.median(list2)
+        prefix = 'Median'
     else:
         val1= statistics.mean(list1)
         val2 = statistics.mean(list2)
+        prefix = 'Mean'
+    
     stdevl1 = statistics.stdev(list1)
     stdevl2 = statistics.stdev(list2)
     
     
 
     
-    print ('\t\tMean l1 : ' + str(val1) + '\tStDev l1: ' + str(statistics.stdev(list1)) + "\tN = "+str(len(list1)) + ' [' +str(list1[np.argmin(list1)]) + ';' + str(list1[np.argmax(list1)]) + ']')
-    print ('\t\tMean l2 : ' + str(val2) + '\tStDev l2: ' + str(statistics.stdev(list2)) + "\tN = "+str(len(list2)) + ' [' +str(list2[np.argmin(list2)]) + ';' + str(list2[np.argmax(list2)]) + ']')
+    print ('\t\t'+prefix+' l1 : ' + str(val1) + '\tStDev l1: ' + str(statistics.stdev(list1)) + "\tN = "+str(len(list1)) + ' [' +str(list1[np.argmin(list1)]) + ';' + str(list1[np.argmax(list1)]) + ']')
+    print ('\t\t'+prefix+' l2 : ' + str(val2) + '\tStDev l2: ' + str(statistics.stdev(list2)) + "\tN = "+str(len(list2)) + ' [' +str(list2[np.argmin(list2)]) + ';' + str(list2[np.argmax(list2)]) + ']')
     
     if (printNorm):
        print (str(len(list1)))
@@ -63,7 +66,7 @@ def CalcNonParametric_MannWhitneyWithShapiro(list1, list2, my_alternative='two-s
 def CalcParametric_WelshWithShapiroAndLevene(list1, list2, printHint=False, printNorm=False):
     if (printNorm):
         print ('\t\tnormality list1 (Shapiro) = ' + str(scipy.stats.shapiro(list1)))
-        print ('\t\tnormality list2 (Shapiro) = ' + str(scipy.stats.shapiro(list2)))
+        print ('\t\tnormality list2 (Shapiro) = ' + str(scipy.stats.shapiro(list2)) )
  
     #print (str(len(list1)))
     #print (str(len(list2)))
@@ -71,9 +74,9 @@ def CalcParametric_WelshWithShapiroAndLevene(list1, list2, printHint=False, prin
     equalvar = scipy.stats.levene(list1, list2, center='mean')
       
     if (equalvar[1] < 0.05):
-        print ('\t\t' + str(scipy.stats.ttest_ind(list1, list2, equal_var=False)) + '; Welch: '+ str(equalvar[1])) #Welch
+        print ('\t\t' + str(scipy.stats.ttest_ind(list1, list2, equal_var=False)) + '; Welch: '+ str(equalvar[1])  ) #Welch
     else: 
-        print ('\t\t' + str(scipy.stats.ttest_ind(list1, list2, equal_var=True))+ '; t-test: '+str(equalvar[1]))
+        print ('\t\t' + str(scipy.stats.ttest_ind(list1, list2, equal_var=True))+ '; t-test: '+str(equalvar[1])  )
         # a negative sign implies tahat the sample mean is less than the hypothesized mean
     
     meanl1 = statistics.mean(list1)
@@ -83,8 +86,8 @@ def CalcParametric_WelshWithShapiroAndLevene(list1, list2, printHint=False, prin
     stdevl2 = statistics.stdev(list2)
   
         
-    print ('\t\tmean l1 : ' + str(statistics.mean(list1)) + '\tStDev l1: ' + str(statistics.stdev(list1)))
-    print ('\t\tmean l2 : ' + str(statistics.mean(list2))+ '\tStDev l2: ' + str(statistics.stdev(list2)))
+    print ('\t\tmean l1 : ' + str(statistics.mean(list1)) + '\tStDev l1: ' + str(statistics.stdev(list1))+ "\tN = "+str(len(list1)))
+    print ('\t\tmean l2 : ' + str(statistics.mean(list2))+ '\tStDev l2: ' + str(statistics.stdev(list2))+ "\tN = "+str(len(list2)))
     
     if (printHint):
         print ('\t\tLevene  : If p < 0.05 indicates a violation of the assumption that variance is equal across groups. ')    
@@ -121,18 +124,19 @@ def plotBarChartWithStdDevDouble(n, means1, means2, stdev1, stdev2, axislist, ax
     ax = fig.add_subplot(111)
     
     yvals = means1#[4, 9,6,9,2]
-    rects1 = ax.bar(left=ind+width, height=yvals, width=newwidth, yerr=stdev2, ecolor='black', error_kw=dict(lw=1, capsize=2, capthick=1), color='#4472c4',edgecolor='none',)
+    rects1 = ax.bar(left=ind+width, yerr = stdev1,height=yvals, width=newwidth,  ecolor='black', error_kw=dict(lw=1, capsize=2, capthick=1), color='#4472c4',edgecolor='none',)
     
     zvals = means2#[1,2,21,1,2]
-    rects2 = ax.bar(left=ind+width*2, height=zvals, width=newwidth,  yerr=stdev2, ecolor='black',error_kw=dict(lw=1, capsize=3, capthick=1), color = '#ed7d31', edgecolor='none')#color='#D3D3D3')#, hatch='..')
+    rects2 = ax.bar(left=ind+width*2, yerr = stdev2,height=zvals, width=newwidth,   ecolor='black',error_kw=dict(lw=1, capsize=3, capthick=1), color = '#ed7d31', edgecolor='none')#color='#D3D3D3')#, hatch='..')
     
     ax.set_ylabel(axistitle)
     ax.set_xticks(ind+width*vlabeloffset)
     ax.set_xticklabels( axislist )
     ax.legend( (rects1[0], rects2[0]), ('2D', 'S3D') ,loc=pos )
     if (bLog):
-        pyplot.yscale('log')
-        pyplot.ylim( (pow(10,-1),pow(10,2)) )
+        ax.set_yscale('symlog')
+        #pyplot.yscale('log',nonposx='clip')
+        #pyplot.ylim( (pow(-10,1),pow(10,2)) )
 
     def autolabel(rects):
         for rect in rects:
